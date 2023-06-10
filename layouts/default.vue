4037ebc64629ed9d757c7f2ed5fb2604d5398c67
VictoriaOkazaki <victoria.timein@gmail.com>
2023-06-10 12:01:00 +0400

    google fonts defer load

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,21 +6,24 @@
     </html>
 </template>
 
-<script setup defer>
+<script setup>
 useHead({
     link: [
         {
             href: 'https://fonts.googleapis.com',
-            rel: 'preconnect'
+            rel: 'preconnect',
+            defer: true
         },
         {
             href: 'https://fonts.gstatic.com',
             rel: 'preconnect',
-            crossorigin: ''
+            crossorigin: '',
+            defer: true
         },
         {
             href: 'https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500&family=Delius+Swash+Caps&display=swap',
-            rel: 'stylesheet'
+            rel: 'stylesheet',
+            defer: true
         },
     ]
 })
